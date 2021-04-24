889b489137cafec4aa538ec2e97a0a5126785eb5
wachidmuslih <wachidmslh@gmail.com>
2021-04-24 13:29:57 +0700

    update

--- README.md
+++
@@ -1,5 +0,0 @@
-# CRUDPHPBootstrap_110
-
-Nama : Muhammad Wachid Muslih Muhtadi
-
-NIM  : `20190140110`
--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost", "root", "Bismillah12", "mahasiswa");
+$koneksi = mysqli_connect("localhost", "root", "", "mahasiswa");
 
 if(mysqli_connect_errno()){
     echo "Koneksi database gagal. Pesan error : ".mysqli_connect_error();
