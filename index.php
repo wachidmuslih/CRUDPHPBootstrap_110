02ae2e1b521f712479beffe444bc8ed9874882ab
wachidmuslih <wachidmslh@gmail.com>
2021-04-24 11:40:30 +0700

    Lint Index

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Data Mahasiswa</title>
 </head>
+
 <body>
     <?php include 'config.php' ?>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -17,7 +20,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ms-auto">
-                    <a class="nav-link active" aria-current="page" href="create.php">Tambah Mahasiswa</a> 
+                    <a class="nav-link active" aria-current="page" href="create.php">Tambah Mahasiswa</a>
                 </div>
             </div>
         </div>
@@ -40,13 +43,23 @@
                 while($data = mysqli_fetch_array($mahasiswa)){
                 ?>
                 <tr>
-                    <td><?php echo $no++; ?></td>
-                    <td><?php echo $data['nama']; ?></td>
-                    <td><?php echo $data['nim']; ?></td>
-                    <td><?php echo $data['alamat']; ?></td>
                     <td>
-                        <a href="edit.php?id=<?php echo $data['id']; ?>" class="btn btn-warning btn-sm text-white">EDIT</a>
-                        <a href="delete.php?id=<?php echo $data['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda yakin akan menghapus data mahasiswa ini ?')">HAPUS</a>
+                        <?php echo $no++; ?>
+                    </td>
+                    <td>
+                        <?php echo $data['nama']; ?>
+                    </td>
+                    <td>
+                        <?php echo $data['nim']; ?>
+                    </td>
+                    <td>
+                        <?php echo $data['alamat']; ?>
+                    </td>
+                    <td>
+                        <a href="edit.php?id=<?php echo $data['id']; ?>"
+                            class="btn btn-warning btn-sm text-white">EDIT</a>
+                        <a href="delete.php?id=<?php echo $data['id']; ?>" class="btn btn-danger btn-sm"
+                            onclick="return confirm('Apakah anda yakin akan menghapus data mahasiswa ini ?')">HAPUS</a>
                     </td>
                 </tr>
                 <?php
@@ -55,6 +68,9 @@
             </tbody>
         </table>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
